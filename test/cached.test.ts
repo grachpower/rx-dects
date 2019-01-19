dc69cad22f13f43603f213c28ebae5107f2258e8
Artyom Andreev <amiscary@gmail.com>
2019-01-20 01:46:10 +0300

    update test cases

--- test/cached.test.ts
+++ test/cached.test.ts
@@ -1,6 +1,6 @@
 import 'jest';
 import { Observable, Observer, forkJoin } from 'rxjs';
-import { concatMap } from 'rxjs/operators';
+import { concatMap, tap } from 'rxjs/operators';
 
 import { CachedObservable } from '../src/public-api';
 
@@ -8,10 +8,10 @@ class TestingClass {
     public evalTimes: number = 0;
 
     @CachedObservable()
-    public featchData(): Observable<string> {
-        return Observable.create((observer: Observer<string>) => {
+    public featchData(): Observable<number> {
+        return Observable.create((observer: Observer<number>) => {
             setTimeout(() => {
-                observer.next('next');
+                observer.next(Math.random());
                 this.evalTimes++;
                 observer.complete();
             }, 100);
@@ -34,14 +34,23 @@ test('Cached decorator should get data from cache after first resolved in sequen
 
 test('Cached decorator should get data from cache after first resolved', () => {
     const testEntity = new TestingClass();
+    let resultOne;
+    let resultTwo;
+    let resultThree;
 
     testEntity.featchData()
         .pipe(
+            tap(result => resultOne = result),
             concatMap(() => testEntity.featchData()),
+            tap(result => resultTwo = result),
             concatMap(() => testEntity.featchData()),
+            tap(result => resultThree = result),
         )
         .subscribe(() => {
             expect(testEntity.evalTimes).toBe(1);
+            expect(resultOne).toBe(resultTwo);
+            expect(resultTwo).toBe(resultThree);
+            expect(resultOne).toBe(resultThree);
         });
 });
 
